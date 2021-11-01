c999b4af66f6ba3ea80e0dd527e8ad30fca8b3f8
yandhi <jacksonellsworth03@gmail.com>
2021-11-01 15:43:52 -0400

    Moved to Gatsby

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-styled-components/gatsby-browser.js'),
+      options: {"plugins":[],"displayName":true,"fileName":true,"minify":true,"namespace":"","transpileTemplateLiterals":true,"topLevelImportPaths":[],"pure":false,"disableVendorPrefixes":false},
+    }]
---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Jacks' Site",
+  },
+  plugins: ["gatsby-plugin-styled-components"],
+};
